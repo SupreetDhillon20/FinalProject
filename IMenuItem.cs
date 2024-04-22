7ad3ea1b8089c1c73d20aa792e476dda2279e3c0
SupreetDhillon20 <148576840+SupreetDhillon20@users.noreply.github.com>
2024-04-22 17:02:39 -0600

    Add files via upload

---
+++ IMenuItem.cs
@@ -0,0 +1,13 @@
+﻿
+namespace RestaurantApp.Components.Shared
+{
+    public interface IMenuItem
+    {
+        string Name { get; set; }
+        decimal Price { get; set; }
+        string ImageUrl { get; set; }
+        int Quantity { get; set; }
+        string Category { get; set; }
+    }
+}
+
---
+++ MenuItemBase.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RestaurantApp.Components.Shared
+{
+    public abstract class MenuItemBase : IMenuItem
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+        public int Quantity { get; set; }
+        public string Category { get; set; }
+    }
+
+    public class MenuItem : MenuItemBase
+    {}
+}
+
